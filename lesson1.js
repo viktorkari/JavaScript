1027e2e29a1f5e2aa8d4da63b1a902a012e35c7b
Виктор <viktorkarlin36@gmail.com>
2025-06-22 23:14:30 +0300

    java

---
+++ lesson1.js
@@ -0,0 +1,30 @@
+let firstName = "Viktor";
+let middleName = "Karlin";
+let lastName = "Genadievich";
+let person = firstName +" "+ middleName +" "+ lastName;
+console.log(person);
+
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+console.log(a,b,c,d,e,f,g,h,i,j);
+
+let l = typeof 100;
+let x = typeof "100";
+let w = typeof true;
+
+console.log(l,x,w);
+
+let Name = prompt("name");
+let patronymic = prompt("surname");
+let age = prompt("age");
+
+console.log(Name, patronymic, age);
+
